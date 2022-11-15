be89457bb4f4768db6df9c85cd0cbfcb48288362
Wen-Heng (Jack) Chung <whchung@gmail.com>
2022-11-15 17:47:08 +0000

    HACK: increase private segment buffer to 6 SGPRs.
    - increase private segment buffer to 6 SGPRs.
    - disable preloading the first 2 kernel arguments to SGPRs.

--- llvm/lib/Target/AMDGPU/AMDGPUArgumentUsageInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUArgumentUsageInfo.cpp
@@ -93,7 +93,8 @@ AMDGPUFunctionArgInfo::getPreloadedValue(
   case AMDGPUFunctionArgInfo::PRIVATE_SEGMENT_BUFFER: {
     return std::make_tuple(PrivateSegmentBuffer ? &PrivateSegmentBuffer
                                                 : nullptr,
-                           &AMDGPU::SGPR_64RegClass, LLT::fixed_vector(2, 32));
+                           //&AMDGPU::SGPR_64RegClass, LLT::fixed_vector(2, 32));
+                           &AMDGPU::SGPR_192RegClass, LLT::fixed_vector(6, 32));
   }
   case AMDGPUFunctionArgInfo::KERNELARG0:
     return std::make_tuple(KernelArg0 ? &KernelArg0 : nullptr,
