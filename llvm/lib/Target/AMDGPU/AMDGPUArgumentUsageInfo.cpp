d1e14c3af661adb108dd0345c00463f57102709d
Wen-Heng (Jack) Chung <whchung@gmail.com>
2023-01-12 11:26:40 -0600

    Remove unused codes.

--- llvm/lib/Target/AMDGPU/AMDGPUArgumentUsageInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUArgumentUsageInfo.cpp
@@ -92,8 +92,6 @@ AMDGPUFunctionArgInfo::getPreloadedValue(
   case AMDGPUFunctionArgInfo::PRIVATE_SEGMENT_BUFFER: {
     return std::make_tuple(PrivateSegmentBuffer ? &PrivateSegmentBuffer
                                                 : nullptr,
-                           //&AMDGPU::SGPR_64RegClass, LLT::fixed_vector(2, 32));
-                           //&AMDGPU::SGPR_192RegClass, LLT::fixed_vector(6, 32));
                            &AMDGPU::SGPR_128RegClass, LLT::fixed_vector(4, 32));
   }
   case AMDGPUFunctionArgInfo::KERNELARG0:
